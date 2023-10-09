4e5e382564f2ad9e1f8d82d11e910185029ea139
pivasblazer <pivasblazer@mail.ru>
2023-10-09 14:45:00 +0300

    Добавьте файлы проекта.

---
+++ NodejsWebApp5/2/server.js
@@ -0,0 +1,7 @@
+const os = require('os');
+const greeting = require('./greeting');
+
+let userName = os.userInfo().username;
+
+console.log(`Дата запроса: ${greeting.date}`);
+console.log(greeting.getMessage(userName));
